6ac6128bc74b9385bf3f3545acf06b74a246c763
alexcueto88 <wonderwomanoftech@gmail.com>
2023-02-19 19:31:17 -0600

    first upload

---
+++ JTQuickTrippin/StoreRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTQuickTrippin
+{
+    class StoreRepository
+    {
+
+        static List<Store> _stores = new List<Store>();
+
+        public List<Store> GetStores()
+
+    {
+
+        return _stores;
+    }
+
+    public static void AddStoreList(Store newStore)
+    {
+            _stores.Add(newStore);
+            var str = Newtonsoft.Json.JsonConvert.SerializeObject(_stores);
+        }
+
+}
+}
